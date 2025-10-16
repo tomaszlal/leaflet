fe22ae0afa1a45c5386b1087580248f7c508141d
Tomasz Lalewicz <lalewicz.tomasz@gmail.com>
2025-10-16 21:29:33 +0200

    next step

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   imports: [],
   templateUrl: './map.html',
   styleUrl: './map.css'
 })
-export class Map implements OnInit {
-  ngOnInit(): void {
-    
+export class Map implements AfterViewInit {
+
+  private mapLeaflet: L.Map = L.map('map');
+
+  private initMap():void {
+    this.mapLeaflet = L.map('map',{
+      center:[ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+  
   }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
 
 }
